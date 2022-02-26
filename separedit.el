twlz0ne/separedit.el

commit dfc2f3c40163a00229b81a563a6722bef59a35fa

fix(help-mode,helpul-mode): solve string editing issue

separedit.el
@@ -4,7 +4,7 @@
 
 ;; Author: Gong Qijian <gongqijian@gmail.com>
 ;; Created: 2019/04/06
-;; Version: 0.3.0
+;; Version: 0.3.24
 ;; Package-Requires: ((emacs "25.1") (dash "2.18") (edit-indirect "0.1.5"))
 ;; URL: https://github.com/twlz0ne/separedit.el
 ;; Keywords: tools languages docs
@@ -1464,11 +1464,17 @@ Block info example:
       (while (pcase-let ((`(,depth ,start . ,_) (syntax-ppss)))
                (if (or (and (zerop depth) (not start))
                        (looking-back "^Value:\\(\n\\|\s\\)" 1))
-                   (throw 'break (bounds-of-thing-at-point 'sexp))
+                   (throw 'break
+                     (if (separedit--point-at-string)
+                         (let ((region (separedit--string-region)))
+                           (list (cons (car region) (cadr region)) 'stringp))
+                       (list (bounds-of-thing-at-point 'sexp) nil)))
                  (goto-char start)))))))
 
 (defun separedit-help-variable-edit-info ()
-  "Return help varible edit info (symbol value-bound type local-buffer) at point."
+  "Return help varible edit info at point.
+
+Each element is in the form of (SYMBOL VALUE-BOUND STRINGP TYPE LOCAL-BUFFER)."
   (unless (eq major-mode 'help-mode)
     (user-error "Not in help buffer"))
   (let* ((symbol (separedit-described-symbol))
@@ -1481,7 +1487,7 @@ Block info example:
                           "^Local in buffer \\([^;]+\\); global value is[\s]?"
                           nil t 1)
                      (match-string-no-properties 1))))
-              (goto-char (car bound))
+              (goto-char (caar bound))
               (backward-char)
               (list (cond
                      ((or (looking-back "^Value:[\s]?" 1)
@@ -1493,7 +1499,7 @@ Block info example:
                       'global))
                     buffer?)))))
     (when (and symbol bound (car type-buffer))
-      `(,symbol ,bound ,@type-buffer))))
+      `(,symbol ,@bound ,@type-buffer))))
 
 (defun separedit-helpful-variable-edit-info ()
   "Return helpful variable edit info (symbol value-bound type local-buffer) at point."
@@ -1503,14 +1509,14 @@ Block info example:
          (bound (separedit-described-value-bound))
          (type-buffer
           (save-excursion
-            (goto-char (car bound))
+            (goto-char (caar bound))
             (forward-line -1)
             (cond ((looking-at "^Value$") (list 'global nil))
                   ((looking-at "^Value in #<buffer \\(.*\\)>$")
                    (list 'local (match-string-no-properties 1)))
                   ((looking-at "^Original Value$") (list 'global nil))))))
     (when (and symbol bound (car type-buffer))
-      `(,symbol ,bound ,@type-buffer))))
+      `(,symbol ,@bound ,@type-buffer))))
 
 ;;; separedit-mode
 
@@ -1939,11 +1945,19 @@ If you just want to check `major-mode', use `derived-mode-p'."
                      (`help-mode (separedit-help-variable-edit-info))
                      (`helpful-mode (separedit-helpful-variable-edit-info))))
              (region (nth 1 info))
+             (strp (and (nth 2 info) t))
              (edit-indirect-after-creation-hook #'separedit--buffer-creation-setup)
              (point-info (separedit--point-info (car region) (cdr region)))
              (edit-indirect-guess-mode-function
               `(lambda (_bufer _beg _end)
+                 (when ,strp
+                   (separedit--remove-escape ,strp))
                  (emacs-lisp-mode)
+                 (set (make-local-variable 'edit-indirect-before-commit-hook)
+                      (append '((lambda ()
+                                  (when ,strp
+                                    (separedit--restore-escape ,strp))))
+                              edit-indirect-before-commit-hook))
                  (separedit--restore-point ,@point-info)
                  (setq-local separedit--inhibit-read-only t)
                  (setq-local separedit--help-variable-edit-info ',info))))

test/separedit-test-helper.el
@@ -132,7 +132,8 @@ EXPECTED is of the form (symbol value type local-buffer)"
                    (buffer-substring-no-properties
                     (car (nth 1 edit-info)) (cdr (nth 1 edit-info)))
                    (nth 2 edit-info)
-                   (nth 3 edit-info)))))))
+                   (nth 3 edit-info)
+                   (nth 4 edit-info)))))))
 
 (define-derived-mode helpful-mode special-mode "Dummy Mode" "For Test")
 
@@ -152,7 +153,8 @@ EXPECTED is of the form (symbol value type local-buffer)"
                    (buffer-substring-no-properties
                     (car (nth 1 edit-info)) (cdr (nth 1 edit-info)))
                    (nth 2 edit-info)
-                   (nth 3 edit-info)))))))
+                   (nth 3 edit-info)
+                   (nth 4 edit-info)))))))
 
 (defun separedit-test--execute-block-edit (init-mode key-sequnce init-data expected-data &optional region-regexps)
   (let ((buf (generate-new-buffer "*init*")))

test/separedit-test.el
@@ -945,7 +945,7 @@ functions displays the tooltip, it should return non-nil and the
 rest are not called.
 
 [back]"
-   '(tooltip-hook "(tooltip-help<|>-tips)" global nil))
+   '(tooltip-hook "(tooltip-help<|>-tips)" nil global nil))
   (--assert-help-value
    "company-backends1 is a variable defined in ‘company.el’.
 Its value is shown below.
@@ -966,7 +966,7 @@ Local in buffer *.py; global value is
 (company-cmake company-capf company-files company-oddmuse company-dabbrev)
 
 [back]"
-   '(company-backends1 "(company-lsp company-capf<|>)" local "*.py"))
+   '(company-backends1 "(company-lsp company-capf<|>)" nil local "*.py"))
   (--assert-help-value
    "company-backends2 is a variable defined in ‘company.el’.
 Its value is shown below.
@@ -987,7 +987,7 @@ Local in buffer *.py; global value is
 (company-cmake company-capf company-files company-oddmuse company-dabbrev)
 
 [back]"
-   '(company-backends2 "(company-cmake company-capf<|> company-files company-oddmuse company-dabbrev)" global "*.py"))
+   '(company-backends2 "(company-cmake company-capf<|> company-files company-oddmuse company-dabbrev)" nil global "*.py"))
   (--assert-help-value
    "company-backends3 is a variable defined in ‘company.el’.
 Its value is shown below.
@@ -1008,7 +1008,25 @@ Local in buffer *.py; global value is
 (company-cmake company-capf<|> company-files company-oddmuse company-dabbrev)
 
 [back]"
-   '(company-backends3 "(company-cmake company-capf<|> company-files company-oddmuse company-dabbrev)" global "*.py")))
+   '(company-backends3 "(company-cmake company-capf<|> company-files company-oddmuse company-dabbrev)" nil global "*.py"))
+  (--assert-help-value
+   "color-rg-search-ignore-rules is a variable defined in ‘color-rg.el’.
+
+Its value is \"-g '!#*'<|> -g '!node_modules' -g '!dist'\"
+Original value was 
+\"-g '!node_modules' -g '!dist'\"
+
+When ‘color-rg-search-no-ignore-file’ is non-nil, color-rg will search any file.
+Include file match gitignore rule.
+
+Default rule is search any file but except ‘node_modules’ and ‘dist’ directory,
+you can customize ignore rules with your like.
+
+  You can customize this variable.
+
+[back]"
+   '(color-rg-search-ignore-rules "-g '!#*'<|> -g '!node_modules' -g '!dist'"
+     stringp global nil)))
 
 (ert-deftest separedit-test-helpful-value-edit-info ()
   (--assert-helpful-value
@@ -1026,7 +1044,7 @@ Documentation
 Functions to call to display tooltips.
 
 ..."
-   '(tooltip-hook "(tooltip-help<|>-tips)" global nil))
+   '(tooltip-hook "(tooltip-help<|>-tips)" nil global nil))
   (--assert-helpful-value
    "company-backends1 is a variable defined in company.el.
 
@@ -1042,7 +1060,7 @@ Documentation
 The list of active backends (completion engines).
 
 ..."
-   '(company-backends1 "(company-lsp company-capf<|>)" local "*.py"))
+   '(company-backends1 "(company-lsp company-capf<|>)" nil local "*.py"))
   (--assert-helpful-value
    "company-backends2 is a variable defined in company.el.
 
@@ -1058,7 +1076,7 @@ Documentation
 The list of active backends (completion engines).
 
 ..."
-   '(company-backends2 "(company-cmake company-capf<|> company-files company-oddmuse company-dabbrev)" global nil)))
+   '(company-backends2 "(company-cmake company-capf<|> company-files company-oddmuse company-dabbrev)" nil global nil)))
 
 (ert-deftest separedit-test-heredoc-region ()
   (let ((heredocs
@@ -1706,8 +1724,8 @@ comment3
     (should (eq 'tar-mode
                 (cdr (assoc "\\.txz\\'" (car (read-from-string
                                               (buffer-substring-no-properties
-                                               (car bound)
-                                               (cdr bound))))))))))
+                                               (caar bound)
+                                               (cdar bound))))))))))
 
 (ert-deftest separedit-test-gnu-style-comment ()
   (let ((init-str (--join\n "/* comment 1"

commit a33a04479fc1d4fa0ee618833965ce9914b9c1f4

fix(help-mode,helpul-mode): solve value editing issue introduced in dfc2f3c

separedit.el
@@ -4,7 +4,7 @@
 
 ;; Author: Gong Qijian <gongqijian@gmail.com>
 ;; Created: 2019/04/06
-;; Version: 0.3.26
+;; Version: 0.3.27
 ;; Package-Requires: ((emacs "25.1") (dash "2.18") (edit-indirect "0.1.5"))
 ;; URL: https://github.com/twlz0ne/separedit.el
 ;; Keywords: tools languages docs
@@ -1457,12 +1457,12 @@ Block info example:
 (defun separedit-help-variable-edit-info ()
   "Return help varible edit info at point.
 
-Each element is in the form of (SYMBOL VALUE-BOUND STRINGP TYPE LOCAL-BUFFER)."
+Each element is in the form of (SYMBOL VALUE-BOUND STRINGP SCOPE LOCAL-BUFFER)."
   (unless (eq major-mode 'help-mode)
     (user-error "Not in help buffer"))
   (let* ((symbol (separedit-described-symbol))
          (bound (separedit-described-value-bound))
-         (type-buffer
+         (scope-buffer
           (save-excursion
             (goto-char (point-min))
             (let ((buffer?
@@ -1481,8 +1481,8 @@ Each element is in the form of (SYMBOL VALUE-BOUND STRINGP TYPE LOCAL-BUFFER)."
                           (looking-back "^Local in buffer \\([^;]+\\); global value is[\s]?" 1))
                       'global))
                     buffer?)))))
-    (when (and symbol bound (car type-buffer))
-      `(,symbol ,@bound ,@type-buffer))))
+    (when (and symbol bound (car scope-buffer))
+      `(,symbol ,@bound ,@scope-buffer))))
 
 (defun separedit-helpful-variable-edit-info ()
   "Return helpful variable edit info (symbol value-bound type local-buffer) at point."
@@ -1536,13 +1536,15 @@ It will override by the key that `separedit' binding in source buffer.")
     (edit-indirect--barf-if-not-indirect)
     (if separedit--help-variable-edit-info
         (let* ((sym (nth 0 separedit--help-variable-edit-info))
-               (typ (nth 2 separedit--help-variable-edit-info))
-               (buf (nth 3 separedit--help-variable-edit-info))
-               (val (car (read-from-string (buffer-string)))))
+               (scp (nth 3 separedit--help-variable-edit-info))
+               (buf (nth 4 separedit--help-variable-edit-info))
+               (val (if (eq 'stringp (nth 2 separedit--help-variable-edit-info))
+                        (substring-no-properties (buffer-string)) 
+                      (list 'quote (car (read-from-string (buffer-string)))))))
           (cond
-           ((and (eq typ 'local) buf) (with-current-buffer buf (eval `(setq-local ,sym ',val))))
-           ((and (eq typ 'global) (not buf)) (eval `(setq-default ,sym ',val)))
-           (t (message "Unknown variable type: %s" typ)))
+           ((and (eq scp 'local) buf) (with-current-buffer buf (eval `(setq-local ,sym ,val))))
+           ((and (eq scp 'global) (not buf)) (eval `(setq-default ,sym ,val)))
+           (t (message "Unknown variable scope: %s" scp)))
           ;; Make sure `edit-indirect--overlay' not be destroyed.
           (when (overlay-buffer edit-indirect--overlay)
             (edit-indirect--commit)))
@@ -1924,10 +1926,10 @@ If you just want to check `major-mode', use `derived-mode-p'."
 (defun separedit-dwim-described-variable ()
   "Edit value of variable at poin in help/helpful buffer."
   (interactive)
-  (-if-let* ((info (pcase major-mode
-                     (`help-mode (separedit-help-variable-edit-info))
-                     (`helpful-mode (separedit-helpful-variable-edit-info))))
-             (region (nth 1 info))
+  (-if-let (info (pcase major-mode
+                   (`help-mode (separedit-help-variable-edit-info))
+                   (`helpful-mode (separedit-helpful-variable-edit-info))))
+      (let* ((region (nth 1 info))
              (strp (and (nth 2 info) t))
              (edit-indirect-after-creation-hook #'separedit--buffer-creation-setup)
              (point-info (separedit--point-info (car region) (cdr region)))
@@ -1944,7 +1946,7 @@ If you just want to check `major-mode', use `derived-mode-p'."
                  (separedit--restore-point ,@point-info)
                  (setq-local separedit--inhibit-read-only t)
                  (setq-local separedit--help-variable-edit-info ',info))))
-      (edit-indirect-region (car region) (cdr region) 'display-buffer)
+        (edit-indirect-region (car region) (cdr region) 'display-buffer))
     (user-error "Not at variable value")))
 
 ;;;###autoload